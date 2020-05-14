580dd305a121482e0f0523be980835c6d9e90f5b
richNovoa <rich@cross.team>
2020-05-14 14:09:24 -0400

    installing libraries

---
+++ loaddata.py
@@ -0,0 +1,5 @@
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
